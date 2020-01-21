97a355f5ae18bc11ffdc8c66f39b10b32293209d
louis baptiste <louisvis@ymail.com>
2020-01-21 20:27:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let message;
+  if (distance <= 400){
+        message = "This one is on me!";
+   }
+   else if (distance >= 2000 && distance <=2500){
+        message = "I will gladly take your thirty bucks.";
+
+   }
+   else {
+     message = "No can do.";
+   }
+
+
+return message
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message;
+  city === "NYC" ? (message = "Ok, sounds good."):(message ="No go.");
+  return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+let message;
+  switch (tip) {
+    case "generous":
+          message = "Thank you so much."
+          break;
+    case "not as generous":
+          message = "Thank you."
+          break;
+    default:
+          message = "Bye."
+          break;
+
+  }
+
+  return message
+
 }
